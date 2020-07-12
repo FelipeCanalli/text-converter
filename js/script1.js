253eaf3f136b9832dd85c17c09e32fdcd4882ddf
Felipe Galvão <canallinho@gmail.com>
2020-07-12 01:31:06 -0300

    Nova funcionalidade adicionada

--- js/script1.js
+++ js/script1.js
@@ -1,18 +1,29 @@
- 
- function contador() {
-  var largura = document.getElementById("entrada").value.length;
+function contador() {
+  // função de contar
+  var texto = document.getElementById("entrada").value;
   document.getElementById(
     "contador"
-  ).innerHTML = `<p>Nº de Caracteres: ${largura}</p>`;
-};
+  ).innerHTML = `<p>Nº de Caracteres: ${texto.length}</p>`;
+  // passando o texto da textarea entrada para a outra textarea
+  // document.getElementById("saida").innerHTML = `${texto}`;
+}
 
 function upperCase() {
   var dados = document.getElementById("entrada").value.toUpperCase();
   // console.log(dados);
-  document.getElementById("saida").innerText = dados;
+  document.getElementById("saida").innerHTML = `${dados}`;
 }
 
 function lowerCase() {
   var dados = document.getElementById("entrada").value.toLowerCase();
-  document.getElementById("saida").innerText = dados;
+  // Utilizando innerHTML e ` ` para respeitar os espaços
+  document.getElementById("saida").innerHTML = `${dados}`;
+}
+
+function capitalizeFisrtLetter() {
+  var dados = document.getElementById("entrada").value;
+  var dados = dados.toLowerCase().replace(/(?:^|\s)\S/g, function (a) {
+    return a.toUpperCase();
+  });
+  document.getElementById("saida").innerHTML = `${dados}`;
 }
